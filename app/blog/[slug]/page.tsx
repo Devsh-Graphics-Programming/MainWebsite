4babc9f9f428a2280c151395432f405a1b94fe37
YasInvolved <szrtxm.op@gmail.com>
2025-01-31 17:06:23 +0100

    blog explorer

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import dynamic from "next/dynamic";
 import fs from 'fs';
 import path from 'path';
+import PostView from './PostView';
+
+const postsDir = path.join(process.cwd(), 'blog')
 
 export default async function Page({params}: {params: Promise<{slug: string}>}) {
     const slug = (await params).slug
 
-    const { default: Post } = await import(`@/blog/${slug}/page.mdx`)
-
     return (
         <main className="flex flex-col items-center container mx-auto p-4">
             <div className="prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-invert font-thin">
-                <Post/>
+                <PostView slug={slug}/>
             </div>
         </main>
     )
@@ -18,9 +18,7 @@ export default async function Page({params}: {params: Promise<{slug: string}>}) 
 
 // this will cause this page to throw 404 if post doesn't exist
 export function generateStaticParams() {
-    const postsDir = path.join(process.cwd(), 'blog')
     const dirNames = fs.readdirSync(postsDir)
-
     return dirNames.map<{slug: string}>((dirName) => ({ slug: dirName }))
 }
 

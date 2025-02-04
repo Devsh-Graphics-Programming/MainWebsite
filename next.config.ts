fc3163b7fe58b3b39c998321ef55b651d418bd54
YasInvolved <szrtxm.op@gmail.com>
2025-02-04 21:39:25 +0100

    remove blog

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import createMDX from '@next/mdx';
-import remarkGfm from "remark-gfm";
-import rehypeStarryNight from "rehype-starry-night";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
@@ -23,11 +20,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: []
-  }
-})
-
-export default withMDX(nextConfig);
+export default nextConfig;

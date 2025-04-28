939aacc7699a5f4757c3b633045408effd200d2a
YasInvolved <szrtxm.op@gmail.com>
2025-04-29 00:23:49 +0200

    change TextBlock component to TextUtils.TextBlock and add TextUtils.Paragraph

--- app/components/TextBlock.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function TextBlock({children}: {children: ReactNode}) {
-    return <div className="prose sm:prose-sm md:prose-md lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-invert font-thin">{children}</div>
-}

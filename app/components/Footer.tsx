f22cb3c48a3295833f685c99d02c3833d5afb995
YasInvolved <szrtxm.op@gmail.com>
2025-07-09 16:45:27 +0200

    remove unused components, move GP links to separate component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
+import GP_Links from "./GP_Links"
 
 export default function footer() {
     return (
-        <div className="sticky botton-0 flex flex-col items-center justify-center p-4 border-t border-t-[#181818]">
-            <div className="flex flex-row items-center gap-1">
-                <Link href="https://graphics-programming.org/webring/frogs/devsh/prev">⬅️</Link>
-                <Link href="https://graphics-programming.org/webring/">
-                    <Image src="https://graphics-programming.org/img/froge.webp" alt="a friendly froge" width={28} height={28}/>
-                </Link>
-                <Link href="https://graphics-programming.org/webring/frogs/devsh/next">➡️</Link>
-            </div>
+        <div className="flex flex-col items-center justify-center p-4 border-t border-t-[#181818]">
+            <GP_Links/>
             <span>This website is a part of GP webring</span>
         </div>
     )

fda1b1ef894428eac51d67a8ca5cac99872ce89f
YasInvolved <szrtxm.op@gmail.com>
2025-07-09 19:09:13 +0200

    shrink the webring links to make them less visible

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,8 @@ import GP_Links from "./GP_Links"
 
 export default function footer() {
     return (
-        <div className="flex flex-col items-center justify-center p-4 border-t border-t-[#181818]">
+        <div className="flex flex-col items-center justify-center p-1 border-t border-t-[#181818]">
             <GP_Links/>
-            <span>This website is a part of GP webring</span>
         </div>
     )
 }

66fa723cb8bd3364c13b33a18c8260f1d60acec0
YasInvolved <szrtxm.op@gmail.com>
2025-07-09 17:23:37 +0200

    improved vulkanised videos grid

--- app/components/VulkanisedGallery.tsx
+++ app/components/VulkanisedGallery.tsx
@@ -1,27 +1,29 @@
-import vulkanised from "@/app/data/vulkanised.json";
+import vulkanisedData from "@/app/data/vulkanised.json";
 import { YouTubeEmbed } from "@next/third-parties/google";
 
-function Gallery({ videos }: { videos: string[] }) {
+function Videos({ ytIdList }: { ytIdList: string[] }) {
     return (
-        <div className="flex flex-col w-full h-full gap-4">
-            {videos.map((video, index) => (
-                <div className="aspect-video w-full" key={index}>
-                    <YouTubeEmbed videoid={video}/>
+        <>
+            {ytIdList.map((ytId, index) => (
+                <div className="w-full aspect-video overflow-auto" key={index}>
+                    <YouTubeEmbed videoid={ytId}/>
                 </div>
             ))}
-        </div>
+        </>
     )
 }
 
 export default function VulkanisedGallery() {
     return (
-        <div className="flex flex-col lg:flex-row justify-center gap-8 w-full">
-            {vulkanised.map((year, index) => (
-                <div className="w-full max-w-[]" key={index}>
-                    <h3 className="my-2">{`Vulkanised ${year.year}`}</h3>
-                    <Gallery videos={year.videos}/>
+        <div className="grid grid-cols-2 gap-4 w-full">
+            {vulkanisedData.map((yearData, index) => (
+                <div key={index}>
+                    <h2>{yearData.year}</h2>
+                    <div className="flex flex-col gap-2">
+                        <Videos ytIdList={yearData.videos}/>
+                    </div>
                 </div>
             ))}
         </div>
-    )
+    )   
 }

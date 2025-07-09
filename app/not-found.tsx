09df87d934990ee04a399d1b6b8f3785decad048
YasInvolved <szrtxm.op@gmail.com>
2025-07-09 19:10:36 +0200

    fix not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-        <main className="flex flex-row items-center h-full justify-center">
+        <main className="flex flex-row items-center h-dvh justify-center">
             <h3>404 | Not Found</h3>
         </main>
     )

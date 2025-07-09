be68e7c4788e0a6668e9d0e1632c7c12961e3bc7
YasInvolved <szrtxm.op@gmail.com>
2025-07-09 19:18:12 +0200

    add missing animations

--- app/nabla/slide.tsx
+++ app/nabla/slide.tsx
@@ -10,7 +10,12 @@ export default function Slide({ children, className }: { children?: React.ReactN
 
     return (
         <AnimatePresence>
-            <motion.section ref={ref} className={`flex flex-col items-center ${className ?? ""}`}>
+            <motion.section 
+                ref={ref}
+                animate={ isInView ? { opacity: 100, x: 0 } : { opacity: 0, x: -100 } }
+                transition={{ duration: 0.5 }}
+                className={`flex flex-col items-center ${className ?? ""}`}
+            >
                 {children}
             </motion.section>
         </AnimatePresence>

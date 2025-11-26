83c7cc743dbc6c25583a7259b75daf6b5cf517ab
YasInvolved <jbaczyk27@gmail.com>
2025-11-26 17:38:31 +0100

    add photos to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,16 @@
 import Link from "next/link"
+import Image, { StaticImageData } from "next/image";
 import { Paragraph, Chapter } from "../components/TextUtils"
 import ContactEmail from "./ContactEmail"
+
+import vulkanised2023_1 from "@/public/vulkanised_photos/2023/2023_1.jpg";
+import vulkanised2023_2 from "@/public/vulkanised_photos/2023/2023_2.jpg";
+import vulkanised2024_1 from "@/public/vulkanised_photos/2024/2024_1.jpg";
+import vulkanised2024_2 from "@/public/vulkanised_photos/2024/2024_2.jpg";
+
+function VulkanisedPhoto({ src, alt }: { src: StaticImageData | string, alt: string }) {
+    return <Image src={src} alt={alt} className="aspect-video w-[400px]"/>
+}
 
 export default function Page() {
     return (
@@ -27,6 +37,25 @@ export default function Page() {
                     availability of consultants and specific expertise required their regular working hours overlap the
                     normal working hours from San Francisco to Sydney.
                 </Paragraph>
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 my-4 gap-4">
+                    <VulkanisedPhoto 
+                        src={vulkanised2023_1} 
+                        alt="Vulkanised photo 1"
+                    />
+                    <VulkanisedPhoto 
+                        src={vulkanised2023_2} 
+                        alt="Vulkanised photo 2"
+                    />
+                    <VulkanisedPhoto 
+                        src={vulkanised2024_1} 
+                        alt="Vulkanised photo 3"
+                    />
+                    <VulkanisedPhoto 
+                        src={vulkanised2024_2} 
+                        alt="Vulkanised photo 4"
+                    />
+                </div>
             </Chapter>
             <Chapter title="Contact">
                 <Paragraph>

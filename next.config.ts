dc5b24bf9967a8d0e24eebb31a92aa947c8713a0
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2026-02-05 08:23:04 +0100

    Use clean URLs without trailing slash

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   images: {
     unoptimized: true,

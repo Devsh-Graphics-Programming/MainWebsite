341a377a9aab584e6edf13ec548041825bd4d4f9
Erfan Ahmadi <ahmadierfan99@gmail.com>
2026-04-28 10:37:28 +0000

    Task start baseline checkpoint for code review

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import GP_Links from "./GP_Links"
-
-export default function footer() {
-    return (
-        <div className="flex flex-col items-center justify-center p-1 border-t border-t-[#181818]">
-            <GP_Links/>
-        </div>
-    )
-}
